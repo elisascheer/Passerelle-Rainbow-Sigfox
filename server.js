19672f8a1b05908dc90ff3442c0450e274ffe143
elisascheer <elisa.scheer@outlook.fr>
2018-01-07 13:29:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+var express = require("express");
+let RainbowSDK = require('rainbow-node-sdk');
+var pg=require("pg");
+var app = express();
+var conString = process.env.DATABASE_URL;
+var client = new pg.Client(conString);
+client.connect();
+
+app.post("/", function(req, res) { 
+	client.query("CREATE TABLE IF NOT EXISTS temperature(id serial primary key,date timestamp not null, device varchar(10) not null,data varchar(24))");
+	console.log("POST");
+    var body = '';
+	req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+            //res.send("data received\n")
+    });
+    req.on('end', function () {
+            var jsonObj = JSON.parse(body);
+            client.query("INSERT INTO temperature(date,device,data) VALUES(now(),$1,$2)",[jsonObj.device,jsonObj.data]);
+            console.log("ID : " +jsonObj.device);
+            console.log("Payload: " +jsonObj.data);
+            res.send("Data saved in the database successfully!\n")
+    });
+    //res.send();
+    //res.end();
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+let options = {
+    "rainbow": {
+        "host": "sandbox",                      // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
+    },
+    "credentials": {
+        "login": process.env.LOG,  // The Rainbow email account to use
+        "password": process.env.PASS,  // The Rainbow associated password to use
+    },
+    "logs": {
+        "enableConsoleLogs": true,              // Default: true
+        "enableFileLogs": false,                // Default: false
+       
+    },
+    "im": {
+        "sendReadReceipt": true   // True to send the 'read' receipt automatically
+    }
+};
+
+// Instantiate the SDK
+let rainbowSDK = new RainbowSDK(options);
+rainbowSDK.events.on('rainbow_onmessagereceived', function(message) {
+    // test if the message comes from a bubble of from a conversation with one participant
+    if(message.type == "chat") {
+        // Send the answer to the bubble
+        console.log("un client fait une demande");
+        if(message.content=="temp"){
+            var query = client.query("SELECT data,date FROM temperature ORDER BY date DESC NULLS LAST,data LIMIT 1 OFFSET 0");
+            query.on("row", function (row, result) {
+            result.addRow(row);
+            });
+            query.on("end", function (result) {
+                messageSent = rainbowSDK.im.sendMessageToJid(JSON.stringify(result.rows, null, "    "), message.fromJid);
+                console.log(JSON.stringify(result.rows, null, "    "));
+                client.end();
+            });
+        }
+        else {
+            messageSent = rainbowSDK.im.sendMessageToJid("Ceci n'est pas une commande", message.fromJid);
+        }
+    }
+    else {
+        // send the answer to the user directly otherwise
+        //messageSent = rainbowSDK.im.sendMessageToJid('The message answer', message.fromJid);
+    }
+});
+
+// Start the SDK
+rainbowSDK.start();
