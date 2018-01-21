fa55850b5bc8458d78fd0c3f74466fe3353d3234
Elisa <elisa.scheer@outlook.fr>
2018-01-21 14:06:34 +0100

    eng

--- server.js
+++ server.js
@@ -7,32 +7,30 @@ var client = new pg.Client(conString);
 client.connect();
 
 
+client.query("CREATE TABLE IF NOT EXISTS temperature(id serial primary key,date timestamp not null, device varchar(10) not null,data varchar(24))");
 app.post("/", function(req, res) { 
-	client.query("CREATE TABLE IF NOT EXISTS temperature(id serial primary key,date timestamp not null, device varchar(10) not null,data varchar(24))");
-	console.log("POST");
+    console.log("POST");
     var body = '';
-	req.on('data', function (data) {
+    req.on('data', function (data) {
             body += data;
-            console.log("Partial body: " + body);
+            console.log(" " + body);
     });
     req.on('end', function () {
             var jsonObj = JSON.parse(body);
             client.query("INSERT INTO temperature(date,device,data) VALUES(now(),$1,$2)",[jsonObj.device,jsonObj.data]);
-            console.log("ID : " +jsonObj.device);
-            console.log("Payload: " +jsonObj.data);
-            res.send("Data saved in the database successfully!\n");
-            res.end();
-
+            //console.log("ID : " +jsonObj.device);
+            //console.log("Payload: " +jsonObj.data);
+            res.send("Data saved in the database successfully!\n")
     });
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
 
 let options = {
     "rainbow": {
-        "host": "sandbox",                      
+        "host": "sandbox",                      // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
     },
     "credentials": {
         "login": process.env.LOG,  // The Rainbow email account to use
@@ -52,22 +50,38 @@ let options = {
 let rainbowSDK = new RainbowSDK(options);
 rainbowSDK.events.on('rainbow_onmessagereceived', function(message) {
     // test if the message comes from a bubble of from a conversation with one participant
+    var chaine=message.content;
     if(message.type == "chat") {
         // Send the answer to the bubble
-        console.log("un client fait une demande");
-        if(message.content=="temp"){
+        //console.log("un client fait une demande");
+        if(chaine=="temp"){
             var query = client.query("SELECT data,date FROM temperature ORDER BY date DESC NULLS LAST,data LIMIT 1 OFFSET 0");
             query.on("row", function (row, result) {
             result.addRow(row);
             });
             query.on("end", function (result) {
-                messageSent = rainbowSDK.im.sendMessageToJid(JSON.stringify(result.rows, null, "    "), message.fromJid);
-                console.log(JSON.stringify(result.rows, null, "    "));
-                //client.end();
+                var send=JSON.stringify(result.rows, null, "");
+                var splitdata=send.split("[");
+                var splitdata_=splitdata[1].split("]");
+                messageSent = rainbowSDK.im.sendMessageToJid("The temperature is "+ JSON.parse(splitdata_[0]).data + "°C", message.fromJid);
             });
         }
+
+        else if(chaine.indexOf("link")==0){ //si on entre la commande link
+            //link ID name
+            client.query("CREATE TABLE IF NOT EXISTS patient(deviceid varchar(24),name varchar(24),doctorid varchar(300))");
+            console.log("Associer un patient à un device\n");
+            var split=chaine.split(" ");
+            if (split.length!=3) messageSent = rainbowSDK.im.sendMessageToJid("usage : link ID name", message.fromJid);
+            console.log(message.fromJid);
+            client.query("INSERT INTO patient(deviceid,name,doctorid) VALUES ($1,$2,$3)",[split[1],split[2],message.fromJid]);
+            //messageSent = rainbowSDK.im.sendMessageToJid("Nouveau patient bien enregistré!", message.fromJid);
+            //console.log(split);
+
+
+        }
         else {
-            messageSent = rainbowSDK.im.sendMessageToJid("Ceci n'est pas une commande", message.fromJid);
+            messageSent = rainbowSDK.im.sendMessageToJid("This is not a command", message.fromJid);
         }
     }
     else {
